66f018a4b9d8b43c997deb4b79e621080575aaa0
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2018-12-20 22:24:39 +0400

    AEReceivers контрол для добавления/изменения Получателей

---
+++ Mail Sender/View/AEReceivers.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mail_Sender.View
+{
+    /// <summary>
+    /// Interaction logic for AEReceivers.xaml
+    /// </summary>
+    public partial class AEReceivers : UserControl
+    {
+        public AEReceivers()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_Error(object sender, ValidationErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
