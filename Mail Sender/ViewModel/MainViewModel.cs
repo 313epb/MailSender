8cd98422446ac3b6767b7984686fc24d64646128
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2018-12-21 18:34:22 +0400

    Перенес данные с DB INitialize во ViewModel

--- Mail Sender/ViewModel/MainViewModel.cs
+++ Mail Sender/ViewModel/MainViewModel.cs
@@ -1,24 +1,161 @@
 using GalaSoft.MvvmLight;
+using MailSender.Domain.Entities;
+using System.Collections.Generic;
+using System;
+using System.Collections.ObjectModel;
+using MailSender.Domain.Entities.Base.Interface;
 
 namespace Mail_Sender.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
+
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        private static List<Mail> _mails = new List<Mail>
+        {
+            new Mail
+            {
+                Id = 0,
+                Content = "Hello World!",
+                Created = DateTime.Now,
+                IsHTML = false,
+                Topic = "ѕервое письмо"
+            },
+            new Mail
+            {
+                Id = 1,
+                Content = "Hello World!",
+                Created = DateTime.Now,
+                IsHTML = true,
+                Topic = "¬торое письмо"
+            },
+            new Mail
+            {
+                Id = 2,
+                Content = "Hello World!",
+                Created = DateTime.Now,
+                IsHTML = true,
+                Topic = "“ретье письмо"
+            },
+            new Mail
+            {
+                Id = 3,
+                Content = "Hello World!",
+                Created = DateTime.Now,
+                IsHTML = false,
+                Topic = "„етвертое письмо"
+            },
+            new Mail
+            {
+                Id = 4,
+                Content = "Hello World!",
+                Created = DateTime.Now,
+                IsHTML = true,
+                Topic = "ѕ€тое письмо"
+            }
+        };
+
+        public static List<Mail> Mails
+        {
+            get => _mails;
+            set => _mails = value;
+        }
+
+        private static ObservableCollection<IPair> _senders = new ObservableCollection<IPair>
+        {
+            new Sender()
+            {
+                Id = 0,
+                Key = "dsderugin@gmail.com",
+                Value = "892asdasdas"
+            },
+            new Sender()
+            {
+                Id = 0,
+                Key = "rusoptsales@gmail.com",
+                Value = "8dasdasdf7506"
+            },
+            new Sender()
+            {
+                Id = 0,
+                Key = "nnderygina@gmail.com",
+                Value = "89sdfssgsg6"
+            }
+        };
+
+        public static ObservableCollection<IPair> Senders
+        {
+            get => _senders;
+            set => _senders = value;
+        }
+
+        private static ObservableCollection<IPair> _smtps = new ObservableCollection<IPair>
+        {
+            new SMTP
+            {
+                Id = 0,
+                Key = "smtp.gmail.com",
+                Value = "587"
+            },
+            new SMTP
+            {
+                Id = 1,
+                Key = "smtp.yandex.ru",
+                Value = "465"
+            },
+            new SMTP
+            {
+                Id = 2,
+                Key = "smtp.mail.ru",
+                Value = "465"
+            }
+        };
+
+        public static ObservableCollection<IPair> SMTPs
+        {
+            get => _smtps;
+            set => _smtps = value;
+        }
+
+        private static ObservableCollection<Receiver> _receivers = new ObservableCollection<Receiver>
+        {
+            new Receiver
+            {
+                Id = 0,
+                Email = "dsderugin@gmail.com",
+                ReceiverName = "ƒмитрий"
+            },
+            new Receiver
+            {
+                Id = 1,
+                Email = "rusoptsales@gmail.com",
+                ReceiverName = "ƒмитрий"
+            },
+            new Receiver
+            {
+                Id = 2,
+                Email = "uchihasaskekun@gmail.com",
+                ReceiverName = "ƒмитрий"
+            },
+            new Receiver
+            {
+                Id = 3,
+                Email = "dsderyugin@gmail.com",
+                ReceiverName = "ƒмитрий"
+            },
+            new Receiver
+            {
+                Id = 4,
+                Email = "deruginds@gmail.com",
+                ReceiverName = "ƒмитрий"
+            },
+        };
+
+        public static ObservableCollection<Receiver> Receivers
+        {
+            get => _receivers;
+            set => _receivers = value;
+        }
+
         public MainViewModel()
         {
             ////if (IsInDesignMode)
