80aa8ecf4357c37c8675eed145b7007db73151ff
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2018-12-24 11:06:33 +0400

    Установил EntityFrameworkCore SQL Server мапим аттрибуты

--- MailSender.Domain/Entities/Base/Interface/IBaseEntity.cs
+++ MailSender.Domain/Entities/Base/Interface/IBaseEntity.cs
@@ -1,8 +1,13 @@
-﻿namespace MailSender.Domain.Entities.Base.Interface
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MailSender.Domain.Entities.Base.Interface
 {
     public interface IBaseEntity
     {
         //Базовая сущность
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int Id { get; set; }
     }
 }
