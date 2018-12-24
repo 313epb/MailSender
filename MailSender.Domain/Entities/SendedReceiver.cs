c0d6c711ca84e0c45411c689178bfd2e788f8abd
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2018-12-24 14:07:34 +0400

    Пересоздаем DAL

--- MailSender.Domain/Entities/SendedReceiver.cs
+++ MailSender.Domain/Entities/SendedReceiver.cs
@@ -1,11 +1,18 @@
-﻿namespace MailSender.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MailSender.Domain.Entities
 {
     public class SendedReceiver
     {
-        public int ReceiverId { get; set; }
+        public int? ReceiverId { get; set; }
+
+        [ForeignKey("ReceiverId")]
         public Receiver Receiver { get; set; }
 
-        public int SendedId { get; set; }
+        public int? SendedId { get; set; }
+
+        [ForeignKey("SendedId")]
         public Sended Sended { get; set; }
     }
 }
