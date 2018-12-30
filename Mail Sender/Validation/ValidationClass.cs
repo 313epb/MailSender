b35ff35650dcd23bd5e0f5db1a77a6a761443807
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2018-12-30 23:58:01 +0400

    Класс написал, осталось проставить валидации. Окно тоже написано.

--- Mail Sender/Validation/ValidationClass.cs
+++ Mail Sender/Validation/ValidationClass.cs
@@ -10,27 +10,26 @@ namespace Mail_Sender.Validation
 {
     public class ValidationClass
     {
-        public object syncObj= new object();
-
         public ValidationResult ValidateMail(Mail item)
         {
             ValidationResult res= new ValidationResult();
             res.result = true;
             res.errorList = new List<string>();
 
-            lock (syncObj)
+            if (item == null)
             {
-                if (item == null)
-                {
-                    res.errorList.Add("Письмо is null");
-                    res.result = false;
-                }
+                res.errorList.Add("Письмо is null");
+                res.result = false;
+            }
+            else
+            {
                 if (string.IsNullOrEmpty(item.Topic))
                 {
                     res.errorList.Add("Тема письма не должна быть пустой.");
                     res.result = false;
                 }
             }
+
             return res;
         }
 
@@ -47,11 +46,14 @@ namespace Mail_Sender.Validation
                 res.errorList.Add($"{item.ClassName} не выбран");
                 res.result = false;
             }
-
-            if (!reg.IsMatch(item.Key))
+            else
             {
-                res.errorList.Add($"Введите корректный {item.KeyName}");
-                res.result = false;
+
+                if (!reg.IsMatch(item.Key))
+                {
+                    res.errorList.Add($"Введите корректный {item.KeyName}");
+                    res.result = false;
+                }
             }
 
             return res;
@@ -69,18 +71,21 @@ namespace Mail_Sender.Validation
                 res.errorList.Add($"{item.ClassName} не выбран");
                 res.result = false;
             }
-
-            if (!reg.IsMatch(item.Key))
+            else
             {
-                res.errorList.Add($"Введите корректный {item.KeyName}");
-                res.result = false;
-            }
+
+                if (!reg.IsMatch(item.Key))
+                {
+                    res.errorList.Add($"Введите корректный {item.KeyName}");
+                    res.result = false;
+                }
 
 
-            if (item.Value.Length<6)
-            {
-                res.errorList.Add("Введенный пароль скорее всего неверен (менее 6ти символов).");
-                res.result = false;
+                if (item.Value.Length < 6)
+                {
+                    res.errorList.Add("Введенный пароль скорее всего неверен (менее 6ти символов).");
+                    res.result = false;
+                }
             }
 
             return res;
@@ -99,24 +104,93 @@ namespace Mail_Sender.Validation
                 res.errorList.Add($"{item.ClassName} не выбран");
                 res.result = false;
             }
-
-            if (!reg.IsMatch(item.Key))
+            else
             {
-                res.errorList.Add($"Введите корректный {item.KeyName}");
-                res.result = false;
+
+                if (!reg.IsMatch(item.Key))
+                {
+                    res.errorList.Add($"Введите корректный {item.KeyName}");
+                    res.result = false;
+                }
+
+                if (!Int32.TryParse(item.Value, out port))
+                {
+                    res.errorList.Add($"Введите корректный {item.ValueName}. Это должно быть число.");
+                    res.result = false;
+                }
+
+                if (port < 100 || port > 999)
+                {
+                    res.errorList.Add($"Введите корректный {item.KeyName}. От 100 до 999");
+                    res.result = false;
+                }
             }
 
-            if (!Int32.TryParse(item.Value, out port))
+            return res;
+        }
+
+        public ValidationResult ValidateSended(Sended item)
+        {
+            ValidationResult res = new ValidationResult();
+            res.result = true;
+            res.errorList = new List<string>();
+
+            if (item==null)
             {
-                res.errorList.Add($"Введите корректный {item.ValueName}. Это должно быть число.");
+                res.errorList.Add("Не передан экземпляр класса");
                 res.result = false;
+            }
+            else
+            {
+                ValidationResult temp;
+                temp = ValidateMail(item.Mail);
+                if (!temp.result)
+                {
+                    foreach (string s in temp.errorList)
+                    {
+                        res.errorList.Add(s);
+                    }
+
+                    res.result = temp.result;
+                }
+
+                foreach (SendedReceiver receiver in item.SendedReceivers)
+                {
+                    temp = ValidateReceiver(receiver.Receiver);
+                    if (!temp.result)
+                    {
+                        foreach (string s in temp.errorList)
+                        {
+                            res.errorList.Add(s);
+                        }
+
+                        res.result = temp.result;
+                    }
+                }
+
+                temp = ValidateSender(item.Sender);
+                if (!temp.result)
+                {
+                    foreach (string s in temp.errorList)
+                    {
+                        res.errorList.Add(s);
+                    }
+
+                    res.result = temp.result;
+                }
+
+                temp = ValidateSMTP(item.SMTP);
+                if (!temp.result)
+                {
+                    foreach (string s in temp.errorList)
+                    {
+                        res.errorList.Add(s);
+                    }
+
+                    res.result = temp.result;
+                }
             }
 
-            if (port<100 || port>999)
-            {
-                res.errorList.Add($"Введите корректный {item.KeyName}. От 100 до 999");
-                res.result = false;
-            }
             return res;
         }
     }
