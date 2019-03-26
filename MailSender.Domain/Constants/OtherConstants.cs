2b7c5582ac2d6ce90593eaf67d19f9d0e589c15b
Dmitrii Deriugin <Dmitrii_Deriugin@epam.com>
2019-03-26 08:51:14 +0400

    Git Bash testing

--- MailSender.Domain/Constants/OtherConstants.cs
+++ MailSender.Domain/Constants/OtherConstants.cs
@@ -5,5 +5,7 @@
         public static string DateTimeFormat {get => "HH:mm:ss dd.MM.yyy";}
 
         public static string ErrorWindowTitle { get=> "Обнаружена ошибка."; }
+
+        public static string GitTest => "Git update";
     }
 }
